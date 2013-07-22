784e10a069f245e23e80f8ab07ae137394cae6dc
Dustin Hendricks <dqhendricks@hotmail.com>
2013-07-22 00:39:41 -0700

    Added note.

--- public_html/user.php
+++ public_html/user.php
@@ -23,6 +23,7 @@
 			if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
 				if ( isset( $_REQUEST['score'] ) ) {
 					// check secondary signature to prevent cheating
+					// should probably copy $_REQUEST, remove signature, then concat sorted by keys to uniformly generate the signed request payload
 					$signed_request = new SignedRequest( $_REQUEST['signature'], $_REQUEST['signed_request'] . $_REQUEST['score'] );
 					if ( $signed_request->isValid() ) {
 						$user->addScore( $_REQUEST['score'] );
