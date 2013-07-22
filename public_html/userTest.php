6fa94d10e9bf720f8514e83f1f2db05bf8fa186f
Dustin Hendricks <dqhendricks@hotmail.com>
2013-07-22 00:28:04 -0700

    Added proper score to user test.

--- public_html/userTest.php
+++ public_html/userTest.php
@@ -8,7 +8,7 @@
 		<form method="post" action="user.php">
 			<div>
 				<p>Score</p>
-				<input name="score" type="number" />
+				<input name="score" type="number" value="10" />
 				<p>Signed Request</p>
 				<textarea name="signed_request">cjv1NZlSRCthYq9rAyWEidD7QE98p0PKZvVwpQ7gPwg.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImV4cGlyZXMiOjEzMjI4NTYwMDAsImlzc3VlZF9hdCI6MTMyMjg1MDc1NCwib2F1dGhfdG9rZW4iOiJBQUFCelMwYVhTMDBCQUlob0I1bmhrYnZJU0xLSGpNb3ZIN2ZTTmMzWkFxbnVNT2NvYmpJUHoxNGFmWXV1dzBkbkZzeVpBV2JHU2MycXZBakdjRzZUQ1RWZzBLOUVGUWJ5WkJwNTU0ZXE5M2FTWkFXZXpVeEYiLCJ1c2VyIjp7ImNvdW50cnkiOiJ1cyIsImxvY2FsZSI6ImVuX1VTIiwiYWdlIjp7Im1pbiI6MjF9fSwidXNlcl9pZCI6IjEwMDAwMzI5MTY2MTkwOSJ9</textarea>
 				<p>Signature:</p>
