55694c52482bb5839c35d4bd41189b0ae8106e61
usernamealreadyis <usernamealreadyis@users.noreply.github.com>
2017-05-07 21:33:40 +0700

    hg.js

---
+++ dumm/test/hg.js
@@ -0,0 +1,29 @@
+/***************************
+This is JavaScript (JS), the programming language that powers the web (and this is a comment, which you can delete).
+To use this file, link it to your markup by placing a <script> in the <body> of your HTML file:
+  <body>
+    <script src="script.js"></script>
+replacing "script.js" with the name of this JS file.
+Learn more about JavaScript at
+https://developer.mozilla.org/en-US/Learn/JavaScript
+***************************/
+-¦¦¥   2 ;  application/AppConstants  java/lang/Object §APP_VERSION_CHECK_URL Ljava/lang/String; 
+ConstantValue 	 .http://www.proxynova.com/version.php?app=suite 
+APP_VERSION  2.1 
+DATE_STRING SOURCES_PATH  
+./sources.txt 
+SOURCES_URL  )http://www.proxynova.com/proxysources.txt NEW_LINE <clinit> ()V Code  java/text/SimpleDateFormat  
+MMMMM-dd-yyyy
+     <init> §(Ljava/lang/String;)V ! java/util/Date
+   #  
+  % & ' format $(Ljava/util/Date;)Ljava/lang/String;	  ) 
+  + line.separator
+ - / . java/lang/System 0 1 
+getProperty &(Ljava/lang/String;)Ljava/lang/String;	  3 ¶  ¤LineNumberTable LocalVariableTable
+  # this Lapplication/AppConstants; 
+SourceFile AppConstants.java !                
+       
+  
+            ¤           ¶      §      C      + YÀ +  YÀ "Â $¦ (*© ,¦ 2¦    4   
+    
+  ¤ 5             /     *À 6¦    4        5        7 8    9    :
